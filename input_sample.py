2b5c18552b133bbcb638cf44c52efce7d763e5fb
enriqueperaltajimenez <enriqueperalta1@icloud.com>
2025-01-21 16:54:50 +0100

    added an input example

---
+++ input_sample.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+print("Hello", name)
+age = input("How old are you?")
+print(name, "you were born in", 2024-int(age))
+#another way too format the print is via f-strings (to be taught)
+print(f"{name}, you were born in {2024-int(age)}")
